eae8a54ff3c653f07d37193f4c38a4acffa1dc7f
minhkhang1008 <minhkhang.0884@gmail.com>
2024-11-17 01:57:33 +0700

    Update instruction
    I was playing around with markdown file and tried to display it beautifully, but I realised html is just much better and easier :v

--- components/MarkdownDisplay.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-
-const MarkdownDisplay = ({ content }) => {
-  return <ReactMarkdown>{content}</ReactMarkdown>;
-};
-
-export default MarkdownDisplay;
