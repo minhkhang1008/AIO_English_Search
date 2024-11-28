6b7e959f0bc7a96e0b7c258783e822da60d85707
minhkhang1008 <minhkhang.0884@gmail.com>
2024-11-28 20:46:26 +0700

    debugging

--- api/firebaseConfig.js
+++ api/firebaseConfig.js
@@ -1,5 +1,10 @@
 export default function handler(req, res) {
     if (req.method === 'GET') {
+        if (!process.env.FIREBASE_API_KEY) {
+            console.error('Missing Firebase API key in environment variables.');
+            return res.status(500).json({ error: 'Firebase configuration missing.' });
+        }
+
         res.status(200).json({
             apiKey: process.env.FIREBASE_API_KEY,
             authDomain: process.env.FIREBASE_AUTH_DOMAIN,
